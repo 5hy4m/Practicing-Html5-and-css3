391181ad53cd9eb3fabd1f3941a0751f0181738c
Shyam <vcsshyam1998@gmail.com>
2021-01-16 17:59:49 +0530

    Desktop Sider and tabs done

---
+++ slick_init.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    
+    $('.yearSlidser').slick({
+            slidesToShow: 7,
+            slidesToScroll: 1,
+             dots: false,
+             arrows:false,
+             lazyLoad: 'ondemand',
+             ease : 'Pow4.easeIn',
+             infinite: false,
+                responsive: [
+                  {
+                    breakpoint: 1024,
+                    settings: {
+                      slidesToShow: 5,
+                      slidesToScroll: 1,
+                      dots: true,
+                    }
+                  },
+                  {
+                    breakpoint: 769,
+                    settings: {
+                      slidesToShow: 4,
+                      slidesToScroll: 2,
+                      centerMode: false,
+                    }
+                  },
+                  {
+                    breakpoint: 480,
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 1,
+                      centerMode: false,
+                    }
+                  }
+    
+                ]
+            });
+
+    $('.slicker').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: false,
+      arrows:false,
+      lazyLoad: 'ondemand',
+      ease : 'Pow4.easeIn',
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: false,
+          }
+        },{
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+          }
+        }
+
+      ]
+  });
+
+          })
